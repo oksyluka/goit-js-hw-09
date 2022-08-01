2d107537308ff167bb9bcf717cb055753c000a6d
oksyluka <oksana_l2@ukr.net>
2022-08-01 17:12:50 +0300

    Task 3 is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const firstDelay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+const submitBtn = document.querySelector('button');
+
+submitBtn.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  for (let i = 0; i < Number(amount.value); i += 1) {
+    let position = i + 1;
+    let delay = Number(firstDelay.value) + i * Number(step.value);
+    
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
