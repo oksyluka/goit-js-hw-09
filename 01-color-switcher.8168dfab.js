276be06093cbf15f48f6015855d87a01b0eb4b10
oksyluka <99077387+oksyluka@users.noreply.github.com>
2022-08-01 14:13:19 +0000

    Deploying to gh-pages from @ oksyluka/goit-js-hw-09@2d107537308ff167bb9bcf717cb055753c000a6d 🚀

---
+++ 01-color-switcher.8168dfab.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(a),n.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.8168dfab.js.map
